77d77316e879b9626891b44ff2c54b4fc52431b0
Younes <m.younes87@gmail.com>
2017-10-10 19:14:08 +0800

    uncomment redis and use actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :conversations, only: [:create] do 
-  	resources :messages, only: [:create]
   	member do
   		post :close
   	end
